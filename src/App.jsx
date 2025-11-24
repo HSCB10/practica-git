cc490fd9b736fa2cb2a4c42e2cc3899ae90c5a3d
Steven <chavadw01@gmail.com>
2025-11-24 16:47:38 -0500

    feat/docs: actualizar App y README

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import CabeceraCV from "./componentes/CabeceraCV";
-import Perfil from "./componentes/perfil";
+import Perfil from "./componentes/Perfil";
 import Experiencia from "./componentes/Experiencia";
 import Educacion from "./componentes/Educacion";
 
+import {
+  datosPersonales,
+  resumenProfesional,
+  listaExperiencias,
+  listaEstudios
+} from "./data";
+
 function App() {
   return (
-    <div style={{ width: "80%", margin: "0 auto", fontFamily: "Arial" }}>
-      <CabeceraCV />
-      <Perfil />
-      <Experiencia />
-      <Educacion />
-    </div>
+    <>
+      <CabeceraCV
+        nombre={datosPersonales.nombre}
+        cargo={datosPersonales.cargo}
+        ciudad={datosPersonales.ciudad}
+        contacto={datosPersonales.contacto}
+      />
+
+      <Perfil resumen={resumenProfesional} />
+
+      <Experiencia experiencias={listaExperiencias} />
+
+      <Educacion estudios={listaEstudios} />
+    </>
   );
 }
 
